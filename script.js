4c56ba018100b64cd0eeb7e3942d1ee4be97e4fa
Uthman <ozzie.housmane@gmail.com>
2025-07-28 17:52:48 +0100

    Changed the logic i change the logic making it less complicated by just multiplying the math.random by 3 then adding 1 to it which makes it generate only just 3 random  number spanning from 1-3 fine tune the conditions too making the first choice "Rock" Printed when randomNumber variabe generte 1 then the next index when it generate 2 and 3

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 console.log("Hello World")
 function getComputerChoice() {
     // getenerate a random number between 1-10
-    let randomNumber = Math.floor(Math.random()* 10) + 1;
+    let randomNumber = Math.floor(Math.random()* 3) + 1;
     console.log(randomNumber);
     // sets computer possible choices
     const choice = ["Rock","Paper", "Scissors" ]
     console.log(choice[1])
-    if (randomNumber <= 4){
+    if (randomNumber === 1){
         console.log(choice[0]);
-    } else if (randomNumber <= 7 ) {
+    } else if (randomNumber === 2 ) {
         console.log(choice[1]);
     } else {
         console.log(choice[2]);
     }
-    
+
 }
 getComputerChoice()
