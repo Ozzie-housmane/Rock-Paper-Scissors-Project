bcd4857b187f8588a7cadaba19de37e7238bf7ea
Uthman <ozzie.housmane@gmail.com>
2025-08-07 22:22:44 +0100

    Update playround code this update the playround code comparing the logic for the different possible value of of humanChoice and computerChoice and logging them into the console describing who won after the comparism although yet to add updated score for each rounds also converted the case of humanChoice and Computerchoice to lowercase solving the equality issue where a sentence case will not be strictly equal to a lower case character

--- script.js
+++ script.js
@@ -1,19 +1,71 @@
 console.log("Hello World")
 function getComputerChoice() {
-    // getenerate a random number between 1-10
+    // getenerate a random number between 1-3
     let randomNumber = Math.floor(Math.random()* 3) + 1;
     console.log(randomNumber);
     // sets computer possible choices
     let choice_1 = "Rock";
-        choice_2 = "paper";
-        choice_3 = "Scissors";
+    let choice_2 = "paper";
+    let choice_3 = "Scissors";
+    // hold value for what computer choice will be in the future
+    let computerChoice = "";
+     // give the computer a choice from the value of random number and stores it in the variable computerChoice
     if (randomNumber === 1){
-        console.log(choice_1);
+        computerChoice = choice_1
+        // console.log(choice_1);
     } else if (randomNumber === 2 ) {
-        console.log(choice_2);
+        computerChoice = choice_2
+        // console.log(choice_2);
     } else {
-        console.log(choice_3);
+        computerChoice = choice_3
+        // console.log(choice_3);
     }
-
+    console.log("Computer choose " + computerChoice)
+    return computerChoice;
 }
 getComputerChoice()
+// Get human choice via prompt and shoiaves it into the humanChoice Variable
+function getHumanChoice() {
+    let humanChoice = prompt("enter your choice");
+    console.log("You choose " + humanChoice);
+    // convert humanChoice to lowercase Character
+    // let humanChoiceCnvt = humanChoice.toLowerCase();
+    // console.log(humanChoiceCnvt);
+    return humanChoice;
+
+}
+// getHumanChoice()
+
+// Keep track of players score 
+let humanScore = 0;
+let computerScore = 0;
+// converts Human and Computer Choices to Lowercase
+function playround (humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    computerChoice = computerChoice.toLowerCase();
+    // sets condition for who win the Rock Paper Scissors game and prints the winner in the console
+    if (humanChoice === computerChoice) {
+        console.log("It's a draw!"); }
+        else if (humanChoice === "rock" && computerChoice === "paper") {
+        console.log("You lose paper beats rock");
+    }   else if (humanChoice === "paper" && computerChoice === "scissors"){
+        console.log("You lose scissors beats paper");
+    }   else if (humanChoice === "scissors" && computerChoice === "rock"){
+        console.log("You lose rock beats scissors");
+    }   else if (humanChoice === "paper" && computerChoice === "rock"){
+        console.log("You win paper beats rock");
+    }   else if (humanChoice === "rock" && computerChoice === "scissors"){
+        console.log("You win rock beats scissors")
+    }   else {
+        console.log("You win scissors beats paper")
+    }
+        
+}
+// pass and stores the return value of human and computer choice into the variable "humanSelection" and "computerSelection"
+ const humanSelection = getHumanChoice();
+ const computerSelection = getComputerChoice();
+// calls playground function to run inputing the return or humanChoice and computerChoice value as arguement
+playround(humanSelection, computerSelection);
+
+
+
